6db80fed519d7c7fae6149d2bbe0bcf2a60fab3d
Takashiro <takashiro@qq.com>
2017-12-16 23:39:04 +0800

    Fix a typo

--- js/client.js
+++ js/client.js
@@ -31,8 +31,8 @@ let $client = new Client();
 (() => {
 	let match = location.href.match(/(^\w+)\:\/\/([^/]+)/i);
 	if (match && match[1] && match[2]) {
-		$client.serverUrl = match[1] + '://' + match[2] + '/?mod=werewolf';
+		$client.serverUrl = match[1] + '://' + match[2] + '/server/?mod=werewolf:';
 	} else {
-		$client.serverUrl = 'server/?mod=werewolf';
+		$client.serverUrl = 'server/?mod=werewolf:';
 	}
 })();
