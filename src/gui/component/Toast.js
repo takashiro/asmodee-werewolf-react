bb7ab80272b57df3cff2d61e3608d6118dc6f053
Takashiro <takashiro@qq.com>
2018-03-08 22:56:06 +0800

    Add Toast

---
+++ src/gui/component/Toast.js
@@ -0,0 +1,45 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Toast extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			visible: false
+		};
+	}
+
+	componentDidMount() {
+		setTimeout(() => {
+			this.toast.classList.add('in');
+		}, 0);
+
+		setTimeout(() => {
+			this.toast.classList.remove('in');
+			this.toast.classList.add('out');
+		}, 900);
+
+		setTimeout(() => {
+			ReactDOM.unmountComponentAtNode(document.getElementById('overlay'));
+		}, 1200);
+	}
+
+	render() {
+		return <div className="toast" ref={toast => { this.toast = toast; }}>
+			{this.props.message}
+		</div>;
+	}
+
+}
+
+Toast.makeToast = message => {
+	ReactDOM.render(
+		<Toast message={message} />,
+		document.getElementById('overlay')
+	);
+};
+
+export default Toast;
