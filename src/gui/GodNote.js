87531479cc9d13a3739d9315d761aa45d100171c
Takashiro <takashiro@qq.com>
2018-03-13 00:05:11 +0800

    Fetch roles from server

--- src/gui/GodNote.js
+++ src/gui/GodNote.js
@@ -4,10 +4,16 @@ import ReactDOM from 'react-dom';
 
 import Room from './Room';
 
+import Role from '../core/Role';
+
+import Toast from './component/Toast';
 import PlayerIcon from './component/PlayerIcon';
 import GameFlow from './component/GameFlow';
 
 import GameRoom from '../game/Room';
+
+import $client from '../net/Client';
+const net = $client.API;
 
 class GodNote extends React.Component {
 
@@ -15,6 +21,7 @@ class GodNote extends React.Component {
 		super(props);
 		this.handleReturn = this.handleReturn.bind(this);
 		this.room = new GameRoom(props.config);
+		this.refreshRoles();
 	}
 
 	handleReturn(e) {
@@ -23,6 +30,53 @@ class GodNote extends React.Component {
 			<Room config={this.props.config} />,
 			document.getElementById('root')
 		);
+	}
+
+	refreshRoles() {
+		// Check if it's opened by room owner
+		const config = this.props.config;
+		let session = config.readSession();
+		if (!session || !session.ownerKey) {
+			return;
+		}
+
+		// Clear current state
+		const room = this.room;
+		const players = room.players;
+		for (let player of players) {
+			player.role = Role.Unknown;
+			player.tags.clear();
+			player.markers.clear();
+		}
+
+		// Send request to server
+		$client.request(net.FetchRoles, {id: config.id, ownerKey: session.ownerKey}, result => {
+			if (typeof result == 'string') {
+				Toast.makeToast(result);
+				return;
+			}
+
+			if (!(result instanceof Array)) {
+				Toast.makeToast('房间已失效，无法刷新。');
+				return;
+			}
+
+			for (let record of result) {
+				let i = record.seat - 1;
+				if (i >= 0 && i < players.length) {
+					let player = players[i];
+					if (record.card) {
+						let card = record.card;
+						if (card.role) {
+							player.role = Role.fromNum(card.role);
+						}
+						if (card.cards) {
+							player.cards = card.cards.map(role => Role.fromNum(role));
+						}
+					}
+				}
+			}
+		});
 	}
 
 	render() {
