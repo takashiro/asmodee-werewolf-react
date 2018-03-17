40c999679b7525d4234e0bc8f2239d9a819481b1
Takashiro <takashiro@qq.com>
2018-03-17 21:42:35 +0800

    Add Villager

--- src/game/SkillList/index.js
+++ src/game/SkillList/index.js
@@ -3,10 +3,12 @@ import Werewolf from './Werewolf';
 import Seer from './Seer';
 import Witch from './Witch';
 import Guard from './Guard';
+import Villager from './Villager';
 
 export default [
 	Werewolf,
 	Seer,
 	Witch,
 	Guard,
+	Villager,
 ];
