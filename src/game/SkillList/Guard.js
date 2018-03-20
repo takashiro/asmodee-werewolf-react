fa2a4e0dcc21ed0fc9d5797dc806008918061abc
Takashiro <takashiro@qq.com>
2018-03-21 00:46:38 +0800

    Simplify guard skill

--- src/game/SkillList/Guard.js
+++ src/game/SkillList/Guard.js
@@ -3,34 +3,18 @@ import Role from '../../core/Role';
 
 import Marker from '../Marker';
 import GameEvent from '../GameEvent';
-import ProactiveSkill from '../ProactiveSkill';
+import MarkerSkill from '../MarkerSkill';
 import PassiveSkill from '../PassiveSkill';
 
 const Guarded = new Marker('Guarded', '守护');
 
 //守卫可以在夜间守护一名玩家
 //TODO: 不能连续两晚守护同一名玩家
-class UseArmor extends ProactiveSkill {
+class UseArmor extends MarkerSkill {
 
 	constructor() {
 		super(Role.Guard, '守护', GameEvent.Night);
-	}
-
-	effect(room, target) {
-		if (!target) {
-			return;
-		}
-
-		if (target.hasMarker(Guarded)) {
-			target.removeMarker(Guarded);
-			return;
-		}
-
-		let prev = room.players.find(target => target.hasMarker(Guarded));
-		if (prev) {
-			prev.removeMarker(Guarded);
-		}
-		target.addMarker(Guarded);
+		this.marker = Guarded;
 	}
 
 }
