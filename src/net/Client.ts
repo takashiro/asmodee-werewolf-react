21cf7368be4d7c8e236c3543fa8115fe4f294b4d
Kazuichi Takashiro <takashiro@qq.com>
2021-01-02 14:04:06 +0800

    Rewrite class Client

---
+++ src/net/Client.ts
@@ -0,0 +1,43 @@
+export default class Client {
+	protected server: string;
+
+	constructor(url = '') {
+		this.server = url;
+	}
+
+	get(api: string): Promise<Response> {
+		return window.fetch(
+			this.server + api,
+			{
+				method: 'GET',
+			},
+		);
+	}
+
+	post(api: string, data: unknown): Promise<Response> {
+		return window.fetch(
+			this.server + api,
+			{
+				method: 'POST',
+				body: JSON.stringify(data),
+			},
+		);
+	}
+
+	delete(api: string): Promise<Response> {
+		return window.fetch(
+			this.server + api,
+			{
+				method: 'DELETE',
+			},
+		);
+	}
+}
+
+let serverUrl = 'http://localhost:2620/';
+let match = location.href.match(/(^\w+)\:\/\/([^/]+)/i);
+if (match && match[1] && match[2]) {
+	serverUrl = match[1] + '://' + match[2] + '/api/';
+}
+
+export const client = new Client(serverUrl);
--- src/net/api.js
+++
@@ -1,8 +0,0 @@
-
-const api = {
-	Room: 'room',
-	Roles: 'roles',
-	Role: 'role',
-};
-
-export default api;
