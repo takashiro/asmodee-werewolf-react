b2c659a1d35e6fab0efea342b5675482d07155a6
Kazuichi Takashiro <takashiro@qq.com>
2021-09-06 22:30:35 +0800

    Translate room page

--- src/gui/Room/index.tsx
+++ src/gui/Room/index.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import {
+	defineMessages,
+	useIntl,
+} from 'react-intl';
 
 import RoleTable from './RoleTable';
 import RoleViewer from './RoleViewer';
@@ -7,6 +11,12 @@ import ShareLink from './ShareLink';
 import RoomModel from '../../model/Room';
 
 import './index.scss';
+
+const msg = defineMessages({
+	roomTitle: { defaultMessage: 'Room Number: {id}' },
+	linkTitle: { defaultMessage: 'Invite Link' },
+	exitButtonText: { defaultMessage: 'Exit' },
+});
 
 interface RoomProps {
 	room: RoomModel;
@@ -17,22 +27,26 @@ function Room(props: RoomProps): JSX.Element {
 		room,
 	} = props;
 
+	const intl = useIntl();
 	const roles = room.getRoles();
 	return (
 		<div className="room">
 			<div className="inline-message">
-				房间号：
-				{room.getId()}
+				{intl.formatMessage(msg.roomTitle, { id: room.getId() })}
 			</div>
 			{roles && <RoleTable roles={roles} />}
 			{!room.isOwner() && <RoleViewer player={room.createPlayer()} />}
 			<div className="box share-link-area">
-				<span className="label">邀请链接</span>
+				<span className="label">
+					{intl.formatMessage(msg.linkTitle)}
+				</span>
 				<ShareLink id={room.getId()} />
 			</div>
 			<div className="button-area">
 				{/* (this.isOwner() ? <button onClick={this.openGodNote}>上帝助手</button> : null) */}
-				<a className="button" href=".">返回</a>
+				<a className="button" href=".">
+					{intl.formatMessage(msg.exitButtonText)}
+				</a>
 			</div>
 		</div>
 	);
