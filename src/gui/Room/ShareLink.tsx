d379dc6495ffd9b6da2d95ee536ba426db732ce4
Kazuichi Takashiro <takashiro@qq.com>
2021-09-06 23:02:00 +0800

    Translate share texts

--- src/gui/Room/ShareLink.tsx
+++ src/gui/Room/ShareLink.tsx
@@ -1,33 +1,44 @@
 import React from 'react';
+import {
+	defineMessage,
+	IntlShape,
+	MessageDescriptor,
+	useIntl,
+} from 'react-intl';
 
-import * as Toast from '../component/Toast';
+import { makeToast } from '../component/Toast';
 
 interface ShareLinkProps {
 	id: number;
 }
 
-const shareTexts: string[] = [
-	'我这次是真的预言家！',
-	'老哥太脏了！',
-	'来啊首刀我啊！',
-	'天黑请闭眼',
-	'出我我就带走你',
-	'谁发言不好我就毒谁',
-	'拍不出身份今天就出你',
-	'一天到晚就知道骗药！',
-	'今晚刀谁呢？',
-	'你该不会是狼王吧？',
-	'你就是铁狼一匹',
-	'加油！吹得快像真的了！',
-	'今天就先出我的查杀',
+const title = defineMessage({ defaultMessage: 'Werewolves of the Miller\'s Hollow' });
+const successfullyCopiedShareLink = defineMessage({ defaultMessage: 'Successfully copied the share link.' });
+const failedToCopyShareLink = defineMessage({ defaultMessage: 'Failed to copy the share link. Please do it manually.' });
+
+const shareTexts: MessageDescriptor[] = [
+	defineMessage({ defaultMessage: 'I am the real seer this time!' }),
+	defineMessage({ defaultMessage: 'Too dirty, bro!' }),
+	defineMessage({ defaultMessage: 'Come on! Attack me tonight!' }),
+	defineMessage({ defaultMessage: 'Close your eyes now.' }),
+	defineMessage({ defaultMessage: 'I\'ll shot you if opted out.' }),
+	defineMessage({ defaultMessage: 'I\'ll poison whoever talks like a werewolf.' }),
+	defineMessage({ defaultMessage: 'Confess your role or get yourself killed.' }),
+	defineMessage({ defaultMessage: 'The weakest werewolves cheat the witch.' }),
+	defineMessage({ defaultMessage: 'Who shall I attack tonight?' }),
+	defineMessage({ defaultMessage: 'You can\'t be the alpha wolf, can you?' }),
+	defineMessage({ defaultMessage: 'You are nothing else but a werewolf.' }),
+	defineMessage({ defaultMessage: 'Make up more! Your lies are almost true!' }),
+	defineMessage({ defaultMessage: 'Opt out the werewolf I saw!' }),
 ];
 
-function copyShareLink(shareLink: string, anchor: HTMLAnchorElement): boolean {
-	const shareText = shareTexts[Math.floor(Math.random() * shareTexts.length)];
+function copyShareLink(intl: IntlShape, shareLink: string, anchor: HTMLAnchorElement): boolean {
+	const titleText = intl.formatMessage(title);
+	const shareText = intl.formatMessage(shareTexts[Math.floor(Math.random() * shareTexts.length)]);
 
 	const linkInput = document.createElement('input');
 	linkInput.type = 'text';
-	linkInput.value = `\uD83D\uDC3A狼人杀 ${shareLink} ${shareText}`;
+	linkInput.value = `\uD83D\uDC3A${titleText} ${shareLink} ${shareText}`;
 	linkInput.contentEditable = 'true';
 	linkInput.readOnly = false;
 
@@ -55,6 +66,7 @@ function copyShareLink(shareLink: string, anchor: HTMLAnchorElement): boolean {
 }
 
 export default function ShareLink(props: ShareLinkProps): JSX.Element {
+	const intl = useIntl();
 	const { id } = props;
 
 	const baseUrl = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
@@ -68,11 +80,11 @@ export default function ShareLink(props: ShareLinkProps): JSX.Element {
 		}
 
 		e.preventDefault();
-		const success = copyShareLink(shareLink, a.current);
+		const success = copyShareLink(intl, shareLink, a.current);
 		if (success) {
-			Toast.makeToast('成功复制该链接。');
+			makeToast(intl.formatMessage(successfullyCopiedShareLink));
 		} else {
-			Toast.makeToast('复制失败。请手动长按该链接，然后分享给好友。');
+			makeToast(intl.formatMessage(failedToCopyShareLink));
 		}
 	}
 
