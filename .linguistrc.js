9fa241ae9423faafeab271795bad3d5835d09517
Kazuichi Takashiro <takashiro@qq.com>
2021-09-07 22:26:38 +0800

    Create dist/message

--- .linguistrc.js
+++ .linguistrc.js
@@ -2,6 +2,11 @@
  * @type {import('@karuta/linguist').Config}
  */
 module.exports = {
-	locales: 'en-US,en-GB,zh-Hans,zh-Hant',
+	locales: [
+		'en-US',
+		'en-GB',
+		'zh-Hans',
+		'zh-Hant',
+	],
 	overrideIdFn: '[sha512:contenthash:base64:6]',
 };
