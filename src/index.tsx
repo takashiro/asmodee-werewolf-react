0127cb8409857a415453116c23df678ceff86ba9
Kazuichi Takashiro <takashiro@qq.com>
2022-11-06 15:33:38 +0800

    Use new APIs of react-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import { createLocale, predictDefaultLanguage } from './model/Locale';
 import App from './gui';
@@ -10,10 +10,14 @@ import './global.scss';
 	const defaultLanguage = predictDefaultLanguage();
 	const defaultLocale = await createLocale(defaultLanguage);
 
-	ReactDOM.render(
+	const container = document.getElementById('root');
+	if (!container) {
+		throw new Error('The HTML template is corrupted.');
+	}
+	const root = createRoot(container);
+	root.render(
 		<App
 			defaultLocale={defaultLocale}
 		/>,
-		document.getElementById('root'),
 	);
 }());
